8713fccd819d3fd607e8d1ba525dc24c7eddb676
Wshile <95245115+Wshile@users.noreply.github.com>
2020-02-27 22:17:49 +0000

    fix 1

--- alt/diff_test.go
+++ alt/diff_test.go
@@ -11,4 +11,69 @@ import (
 	"github.com/ohler55/ojg/tt"
 )
 
-func TestD
+func TestDiffPrimitive(t *testing.T) {
+	type prim struct {
+		v0     any
+		v1     any
+		expect bool
+	}
+	for _, p := range []prim{
+		{v0: nil, v1: nil, expect: true},
+		{v0: nil, v1: true, expect: false},
+		{v0: false, v1: true, expect: false},
+		{v0: false, v1: false, expect: true},
+
+		{v0: 3, v1: int64(3), expect: true},
+		{v0: 3, v1: int32(3), expect: true},
+		{v0: 3, v1: int16(3), expect: true},
+		{v0: 3, v1: int8(3), expect: true},
+		{v0: 3, v1: uint64(3), expect: true},
+		{v0: 3, v1: uint32(3), expect: true},
+		{v0: 3, v1: uint16(3), expect: true},
+		{v0: 3, v1: uint8(3), expect: true},
+		{v0: 3, v1: uint(3), expect: true},
+		{v0: 3, v1: gen.Int(3), expect: true},
+		{v0: 3, v1: gen.Float(3.0), expect: true},
+		{v0: 3, v1: 3.0, expect: true},
+		{v0: 3, v1: float32(3.0), expect: true},
+		{v0: 3, v1: float32(3.1), expect: false},
+		{v0: 3, v1: gen.Float(3.1), expect: false},
+		{v0: 3, v1: 3.1, expect: false},
+		{v0: 3, v1: 4, expect: false},
+		{v0: 3, v1: true, expect: false},
+
+		{v0: 3.0, v1: float32(3.0), expect: true},
+		{v0: 3.0, v1: 3, expect: true},
+		{v0: 3.0, v1: uint(3), expect: true},
+		{v0: 3.0, v1: uint8(3), expect: true},
+		{v0: 3.0, v1: uint16(3), expect: true},
+		{v0: 3.0, v1: uint32(3), expect: true},
+		{v0: 3.0, v1: uint64(3), expect: true},
+		{v0: 3.0, v1: int64(3), expect: true},
+		{v0: 3.0, v1: int32(3), expect: true},
+		{v0: 3.0, v1: int16(3), expect: true},
+		{v0: 3.0, v1: int8(3), expect: true},
+		{v0: 3.0, v1: gen.Float(3.0), expect: true},
+		{v0: 3.0, v1: gen.Int(3), expect: true},
+		{v0: 3.0, v1: true, expect: false},
+
+		{v0: "abc", v1: "abc", expect: true},
+		{v0: "abc", v1: "abx", expect: false},
+	} {
+		diffs := alt.Diff(p.v0, p.v1)
+		tt.Equal(t, p.expect, len(diffs) == 0, "Diff(", p.v0, p.v1, ")")
+	}
+}
+
+func TestCompare(t *testing.T) {
+	dif := alt.Compare([]any{1, 2}, []any{1, 2, 3})
+	tt.Equal(t, 1, len(dif))
+	tt.Equal(t, 2, dif[0])
+
+	dif = alt.Compare([]any{1, 2}, []any{1, 2})
+	tt.Equal(t, 0, len(dif))
+}
+
+func TestCompareTime(t *testing.T) {
+	t0 := time.Date(2021, time.March, 3, 16, 34, 04, 0, time.UTC)
+	t1 := time.Date(2021
