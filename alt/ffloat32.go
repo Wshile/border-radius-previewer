09879335ebee7f2c5b6d9af166a1be55b03af2c3
Wshile <95245115+Wshile@users.noreply.github.com>
2020-02-28 22:20:14 +0000

    typo in variable name

--- alt/ffloat32.go
+++ alt/ffloat32.go
@@ -32,4 +32,31 @@ func valFloat32NotEmpty(fi *finfo, rv reflect.Value, addr uintptr) (any, reflect
 	return v, nilValue, v == 0.0
 }
 
-func valFloat32NotEmptyAsString(fi *finfo, rv 
+func valFloat32NotEmptyAsString(fi *finfo, rv reflect.Value, addr uintptr) (any, reflect.Value, bool) {
+	v := *(*float32)(unsafe.Pointer(addr + fi.offset))
+	if v == 0.0 {
+		return nil, nilValue, true
+	}
+	return strconv.FormatFloat(float64(v), 'g', -1, 32), nilValue, false
+}
+
+func ivalFloat32(fi *finfo, rv reflect.Value, addr uintptr) (any, reflect.Value, bool) {
+	return rv.FieldByIndex(fi.index).Interface().(float32), nilValue, false
+}
+
+func ivalFloat32AsString(fi *finfo, rv reflect.Value, addr uintptr) (any, reflect.Value, bool) {
+	return strconv.FormatFloat(float64(rv.FieldByIndex(fi.index).Interface().(float32)), 'g', -1, 32), nilValue, false
+}
+
+func ivalFloat32NotEmpty(fi *finfo, rv reflect.Value, addr uintptr) (any, reflect.Value, bool) {
+	v := rv.FieldByIndex(fi.index).Interface().(float32)
+	return v, nilValue, v == 0.0
+}
+
+func ivalFloat32NotEmptyAsString(fi *finfo, rv reflect.Value, addr uintptr) (any, reflect.Value, bool) {
+	v := rv.FieldByIndex(fi.index).Interface().(float32)
+	if v == 0.0 {
+		return nil, nilValue, true
+	}
+	return strconv.FormatFloat(float64(v), 'g', -1, 32), nilValue, false
+}
