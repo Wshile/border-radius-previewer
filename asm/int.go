602d0886b38f4823f5e384f81f6516d7c4b42eb3
Wshile <95245115+Wshile@users.noreply.github.com>
2020-03-25 21:11:24 +0000

    Fixing travis.yml

--- asm/int.go
+++ asm/int.go
@@ -19,4 +19,21 @@ possible nil is returned.`,
 
 func intEval(root map[string]any, at any, args ...any) (i any) {
 	if len(args) != 1 {
-		panic(fmt.Er
+		panic(fmt.Errorf("int expects exactly one argument. %d given", len(args)))
+	}
+	switch v := evalArg(root, at, args[0]).(type) {
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		i, _ = asInt(v)
+	case float32, float64:
+		f, _ := asFloat(v)
+		i = int64(f)
+	case string:
+		var err error
+		if i, err = strconv.ParseInt(v, 10, 64); err != nil {
+			i = nil
+		}
+	case time.Time:
+		i = v.UnixNano()
+	}
+	return
+}
---
+++ asm/join_test.go
@@ -0,0 +1,50 @@
+// Copyright (c) 2021, Peter Ohler, All rights reserved.
+
+package asm_test
+
+import (
+	"testing"
+
+	"github.com/ohler55/ojg/asm"
+	"github.com/ohler55/ojg/sen"
+	"github.com/ohler55/ojg/tt"
+)
+
+func TestJoin(t *testing.T) {
+	root := testPlan(t,
+		`[
+           [set $.asm.a [join [a b c] '+']]
+           [set $.asm.b [join [a b c]]]
+         ]`,
+		"{src: []}",
+	)
+	opt := sopt
+	opt.Indent = 2
+	tt.Equal(t,
+		`{
+  a: a+b+c
+  b: abc
+}`, sen.String(root["asm"], &opt))
+}
+
+func TestJoinArgCount(t *testing.T) {
+	p := asm.NewPlan([]any{
+		[]any{"join", []any{}, "x", 1},
+	})
+	err := p.Execute(map[string]any{})
+	tt.NotNil(t, err)
+}
+
+func TestJoinArgType(t *testing.T) {
+	p := asm.NewPlan([]any{
+		[]any{"join", 1, "x"},
+	})
+	err := p.Execute(map[string]any{})
+	tt.NotNil(t, err)
+}
+
+func TestJoinArgType2(t *testing.T) {
+	p := asm.NewPlan([]any{
+		[]any{"join", []any{}, 1},
+	})
+	err := p.Execute(map[string
