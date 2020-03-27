9e66b677dd3497b6dbae62fa7f1562f26ca4ebe4
Wshile <95245115+Wshile@users.noreply.github.com>
2020-03-27 15:06:58 +0000

    edited untitled.html

--- asm/null.go
+++ asm/null.go
@@ -10,4 +10,20 @@ func init() {
 	Define(&Fn{
 		Name: "null?",
 		Eval: null,
-		Desc: `Returns true if the single
+		Desc: `Returns true if the single required argumement is null (JSON)
+or nil (golang) otherwise false is returned.`,
+	})
+	Define(&Fn{
+		Name: "nil?",
+		Eval: null,
+		Desc: `Returns true if the single required argumement is null (JSON)
+or nil (golang) otherwise false is returned.`,
+	})
+}
+
+func null(root map[string]any, at any, args ...any) any {
+	if len(args) != 1 {
+		panic(fmt.Errorf("null? / nil? expects exactly one arguments. %d given", len(args)))
+	}
+	return evalArg(root, at, args[0]) == nil
+}
---
+++ asm/or.go
@@ -0,0 +1,35 @@
+
+// Copyright (c) 2021, Peter Ohler, All rights reserved.
+
+package asm
+
+import (
+	"fmt"
+)
+
+func init() {
+	Define(&Fn{
+		Name: "or",
+		Eval: or,
+		Desc: `Returns true if any of the argument evaluate to true. Any
+arguments that do not evaluate to a boolean or null (false)
+raise an error.`,
+	})
+}
+
+func or(root map[string]any, at any, args ...any) any {
+	val := false
+	for _, arg := range args {
+		switch tv := evalArg(root, at, arg).(type) {
+		case nil:
+		case bool:
+			val = tv
+		default:
+			panic(fmt.Errorf("or expects only boolean arguments. %T is not a boolean", tv))
+		}
+		if val {
+			break
+		}
+	}
+	return val
+}
