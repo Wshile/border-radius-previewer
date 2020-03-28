5cf1013b7955841eca32c7ea41ee67a7501a74e6
Wshile <95245115+Wshile@users.noreply.github.com>
2020-03-28 14:14:12 +0000

    working!

--- asm/product_test.go
+++ asm/product_test.go
@@ -11,4 +11,32 @@ import (
 )
 
 func TestProduct(t *testing.T) {
-	root := test
+	root := testPlan(t,
+		`[
+           [set $.asm.a [product 1 2 3]]
+           [set $.asm.b [* 2.2 2]]
+           [set $.asm.c [* 2.2 2.5]]
+           [set $.asm.d [product]]
+           [set $.asm.e [product 1 2.3]]
+         ]`,
+		"{src: []}",
+	)
+	opt := sopt
+	opt.Indent = 2
+	tt.Equal(t,
+		`{
+  a: 6
+  b: 4.4
+  c: 5.5
+  d: 0
+  e: 2.3
+}`, sen.String(root["asm"], &opt))
+}
+
+func TestProductArgType(t *testing.T) {
+	p := asm.NewPlan([]any{
+		[]any{"product", 1, true},
+	})
+	err := p.Execute(map[string]any{})
+	tt.NotNil(t, err)
+}
