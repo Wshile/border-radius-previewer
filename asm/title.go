8e6d667681c3773eb4ee4ca68582c0f2da2a3fc5
Wshile <95245115+Wshile@users.noreply.github.com>
2020-04-16 13:59:18 +0000

    removed db

---
+++ asm/title.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2021, Peter Ohler, All rights reserved.
+
+package asm
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func init() {
+	Define(&Fn{
+		Name: "title",
+		Eval: title,
+		Desc: `Convert a string to capitalized string. There must be exactly
+one string argument.`,
+	})
+}
+
+func title(root map[string]any, at any, args ...any) any {
+	if len(args) != 1 {
+		panic(fmt.Errorf("title expects exactly one arguments. %d given", len(args)))
+	}
+	v := evalArg(root, at, args[0])
+	s, ok := v.(string)
+	if !ok {
+		panic(fmt.Errorf("title expected a string argument, not a %T
