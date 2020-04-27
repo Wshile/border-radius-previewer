6c47aa537ad93363692b0bc6b59f3adbcb74653d
Wshile <95245115+Wshile@users.noreply.github.com>
2020-04-27 13:47:14 +0000

    Fix locales

---
+++ cmd/benchmarks/alt.go
@@ -0,0 +1,28 @@
+
+// Copyright (c) 2020, Peter Ohler, All rights reserved.
+
+// Package main is the main package. (stupid comment to satisfy the linter).
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/ohler55/ojg/alt"
+)
+
+func altGenerify(b *testing.B) {
+	tm := time.Date(2020, time.April, 12, 16, 34, 04, 123456789, time.UTC)
+	for n := 0; n < b.N; n++ {
+		native := benchmarkData(tm)
+		_ = alt.Generify(native)
+	}
+}
+
+func altGenAlter(b *testing.B) {
+	tm := time.Date(2020, time.April, 12, 16, 34, 04, 123456789, time.UTC)
+	for n := 0; n < b.N; n++ {
+		native := benchmarkData(tm)
+		_ = alt.GenAlter(native)
+	}
+}
---
+++ cmd/benchmarks/gen.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2020, Peter Ohler, All rights reserved.
+
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/ohler55/ojg/gen"
+)
+
+func genParse(b *testing.B) {
+	sample, _ := ioutil.ReadFile(filename)
+	b.ResetTimer()
+	p := &gen.Parser{}
+	for n := 0; n < b.N; n++ {
+		if _, err := p.Parse(sample); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func genParseReuse(b *testing.B) {
+	sample, _ := ioutil.ReadFile(filename)
+	b.ResetTimer()
+	p := &gen.Parser{Reuse: true}
+	for n := 0; n < b.N; n++ {
+		if _, err := p.Parse(sample); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func genParseReader(b *testing.B) {
+	var p gen.Parser
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Failed to read %s. %s\n", filename, err)
+	}
+	defer func() { _ = f.Close() }()
+	for n := 0; n < b.N; n++ {
+		_, _ = f.Seek(0, 0)
+		if _, err = p.ParseReader(f); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func genParseReaderReuse(b *testing.B) {
+	p := gen.Parser{Reuse: true}
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Failed 
