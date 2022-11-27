59e5d271a6cdd87b7db49b77d68d54ff34f9d66b
Wshile <95245115+Wshile@users.noreply.github.com>
2022-11-27 09:46:47 +0000

    fix: Gemfile & Gemfile.lock to reduce vulnerabilities

--- cmd/oj/main.go
+++ cmd/oj/main.go
@@ -69,4 +69,37 @@ func init() {
 	flag.BoolVar(&bright, "b", bright, "bright color")
 	flag.BoolVar(&omit, "o", omit, "omit nil and empty")
 	flag.BoolVar(&wrapExtract, "w", wrapExtract, "wrap extracts in an array")
-	flag.BoolVar(&lazy, "z", lazy, "lazy mode accepts
+	flag.BoolVar(&lazy, "z", lazy, "lazy mode accepts Simple Encoding Notation (quotes and commas mostly optional)")
+	flag.BoolVar(&senOut, "sen", senOut, "output in Simple Encoding Notation")
+	flag.BoolVar(&tab, "t", tab, "indent with tabs")
+	flag.Var(&exValue{}, "x", "extract path")
+	flag.Var(&matchValue{}, "m", "match equation/script")
+	flag.Var(&delValue{}, "d", "delete path")
+	flag.BoolVar(&showVersion, "version", showVersion, "display version and exit")
+	flag.StringVar(&planDef, "a", planDef, "assembly plan or plan file using @<plan>")
+	flag.BoolVar(&showRoot, "r", showRoot, "print root if an assemble plan provided")
+	flag.StringVar(&prettyOpt, "p", prettyOpt, `pretty print with the width, depth, and align as <width>.<max-depth>.<align>`)
+	flag.BoolVar(&html, "html", html, "output colored output as HTML")
+	flag.BoolVar(&safe, "safe", safe, "escape &, <, and > for HTML inclusion")
+	flag.StringVar(&confFile, "f", confFile, "configuration file (see -help-config), - indicates no file")
+	flag.BoolVar(&showFnDocs, "fn", showFnDocs, "describe assembly plan functions")
+	flag.BoolVar(&showFnDocs, "help-fn", showFnDocs, "describe assembly plan functions")
+	flag.BoolVar(&showFilterDocs, "help-filter", showFilterDocs, "describe filter operators like [?(@.x == 3)]")
+	flag.BoolVar(&showConf, "help-config", showConf, "describe .oj-config.sen format")
+	flag.BoolVar(&mongo, "mongo", mongo, "parse mongo Javascript output")
+	flag.StringVar(&convName, "conv", convName, `apply converter before writing. Supported values are:
+  nano - converts integers over 946684800000000000 (2000-01-01) to time
+  rcf3339 - converts string in RFC3339 or RFC3339Nano to time
+  mongo - converts mongo wrapped values e.g.,  {$numberLong: "123"} => 123
+  <with-numbers> - if digits are included then time layout is assumed
+  <other> - any other is taken to be a key in a map with a string or nano time
+`)
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `
+usage: %s [<options>] [@<extraction>]... [(<match>)]... [<json-file>]...
+
+The default behavior it to write the JSON formatted according to the color
+options and the indentation option. If no files are specified JSON 
