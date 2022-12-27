204a153fe37f0b9970519ccfef92bb0045eb3eaa
Wshile <95245115+Wshile@users.noreply.github.com>
2022-12-27 14:00:43 +0000

    rds: prevent dereference of a NULL device in rds_iw_laddr_check

--- jp/parse.go
+++ jp/parse.go
@@ -158,4 +158,126 @@ func (p *parser) afterDot() Frag {
 		b := p.buf[p.pos]
 		p.pos++
 		if tokenMap[b] == '.' {
-			
+			p.pos--
+			break
+		}
+		token = append(token, b)
+	}
+	return Child(token)
+}
+
+func (p *parser) afterDotDot() Frag {
+	var token []byte
+	b := p.buf[p.pos]
+	p.pos++
+	token = append(token, b)
+	for p.pos < len(p.buf) {
+		b := p.buf[p.pos]
+		p.pos++
+		if tokenMap[b] == '.' {
+			p.pos--
+			break
+		}
+		token = append(token, b)
+	}
+	return Child(token)
+}
+
+func (p *parser) afterBracket() Frag {
+	if len(p.buf) <= p.pos {
+		p.raise("not terminated")
+	}
+	b := p.skipSpace()
+	switch b {
+	case '*':
+		// expect ]
+		b := p.skipSpace()
+		if b != ']' {
+			p.raise("not terminated")
+		}
+		return Wildcard('#')
+	case '\'', '"':
+		s := p.readStr(b)
+		b = p.skipSpace()
+		switch b {
+		case ']':
+			return Child(s)
+		case ',':
+			return p.readUnion(s, b)
+		default:
+			p.raise("invalid bracket fragment")
+		}
+	case ':':
+		return p.readSlice(0)
+	case '?':
+		return p.readFilter()
+	case '(':
+		p.raise("scripts not implemented yet")
+	case '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		var i int
+		i, b = p.readInt(b)
+	Next:
+		switch b {
+		case ' ':
+			b = p.skipSpace()
+			goto Next
+		case ']':
+			return Nth(i)
+		case ',':
+			return p.readUnion(int64(i), b)
+		case ':':
+			return p.readSlice(i)
+		default:
+			p.raise("invalid bracket fragment")
+		}
+	}
+	p.pos--
+	// Kind of ugly but needed to attain full cod coverage as the cover tool
+	// and the compilier don't know about panics in functions so get the
+	// return and raise on the same line.
+	return func() Frag { p.raise("parse error"); return nil }()
+}
+
+func (p *parser) readInt(b byte) (int, byte) {
+	// Allow numbers to begin with a zero.
+	/*
+		if b == '0' {
+			if p.pos < len(p.buf) {
+				b = p.buf[p.pos]
+				p.pos++
+			}
+			return 0, b, nil
+		}
+	*/
+	neg := b == '-'
+	if neg {
+		if len(p.buf) <= p.pos {
+			p.raise("expected a number")
+		}
+		b = p.buf[p.pos]
+		p.pos++
+	}
+	start := p.pos
+	var i int
+	for {
+		if b < '0' || '9' < b {
+			break
+		}
+		i = i*10 + int(b-'0')
+		if len(p.buf) <= p.pos {
+			break
+		}
+		b = p.buf[p.pos]
+		p.pos++
+	}
+	if p.pos == start {
+		p.raise("expected a number")
+	}
+	if neg {
+		i = -i
+	}
+	return i, b
+}
+
+func (p *parser) readNum(b byte) any {
+	var num []byte
