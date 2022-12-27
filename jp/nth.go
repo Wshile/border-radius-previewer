ea0f431ff3aefbd77b9d23c28daa5925b778451d
Wshile <95245115+Wshile@users.noreply.github.com>
2022-12-27 08:19:09 +0000

    dynamic fsync: favor true case since most will be using this feature

--- jp/nth.go
+++ jp/nth.go
@@ -45,4 +45,43 @@ func (f Nth) remove(value any) (out any, changed bool) {
 	switch tv := value.(type) {
 	case []any:
 		if i < 0 {
-			i = len(
+			i = len(tv) + i
+		}
+		if 0 <= i && i < len(tv) {
+			out = append(tv[:i], tv[i+1:]...)
+			changed = true
+		}
+	case gen.Array:
+		if i < 0 {
+			i = len(tv) + i
+		}
+		if 0 <= i && i < len(tv) {
+			out = append(tv[:i], tv[i+1:]...)
+			changed = true
+		}
+	default:
+		if rt := reflect.TypeOf(value); rt != nil {
+			if rt.Kind() == reflect.Slice {
+				rv := reflect.ValueOf(value)
+				cnt := rv.Len()
+				if 0 < cnt {
+					if i < 0 {
+						i = cnt + i
+					}
+					if 0 <= i && i < cnt {
+						nv := reflect.MakeSlice(rt, cnt-1, cnt-1)
+						for j := 0; j < i; j++ {
+							nv.Index(j).Set(rv.Index(j))
+						}
+						for j := i + 1; j < cnt; j++ {
+							nv.Index(j - 1).Set(rv.Index(j))
+						}
+						out = nv.Interface()
+						changed = true
+					}
+				}
+			}
+		}
+	}
+	return
+}
---
+++ jp/parse.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2020, Peter Ohler, All rights reserved.
+
+package jp
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+	"strconv"
+
+	"github.com/ohler55/ojg"
+)
+
+const (
+	//   0123456789abcdef0123456789abcdef
+	tokenMap = "" +
+		"................................" + // 0x00
+		"...o.o..........oooooooooooo...o" + // 0x20
+		".oooooooooooooooooooooooooo...oo" + // 0x40
+		".oooooooooooooooooooooooooooooo." + // 0x60
+		"oooooooooooooooooooooooooooooooo" + // 0x80
+		"oooooooooooooooooooooooooooooooo" + // 0xa0
+		"oooooooooooooooooooooooooooooooo" + // 0xc0
+		"oooooooooooooooooooooooooooooooo" //   0xe0
+
+	// o for an operatio
+	// v for a value start character
+	//   0123456789abcdef0123456789abcdef
+	eqMap = "" +
+		"................................" + // 0x00
+		".ov.v.ovv.oo.o.ovvvvvvvvvv..ooo." + // 0x20
+		"v..............................." + // 0x40
+		".....ov.oo....v.....v.......o.o." + // 0x60
+		"................................" + // 0x80
+		"................................" + // 0xa0
+		"................................" + // 0xc0
+		"................................" //   0xe0
+)
+
+// Performance is less a concern with Expr parsing as it is usually done just
+// once if performance is important. Alternatively, an Expr can be built using
+// function calls or bare structs. Parsing is more for convenience. Using this
+// approach over modes only adds 10% so a reasonable penalty for
+/
