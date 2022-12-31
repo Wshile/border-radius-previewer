9be6f955edcacbefe71cd81cafca307a24e7e977
Wshile <95245115+Wshile@users.noreply.github.com>
2022-12-31 10:30:34 +0000

    v0.10

--- jp/script.go
+++ jp/script.go
@@ -134,4 +134,90 @@ func (s *Script) Append(buf []byte) []byte {
 				left = bstack[i+1]
 			}
 			if 2 < len(bstack)-i {
-				right = bsta
+				right = bstack[i+2]
+			}
+			bstack[i] = s.appendOp(o, left, right)
+			if i+int(o.cnt)+1 <= len(bstack) {
+				copy(bstack[i+1:], bstack[i+int(o.cnt)+1:])
+			}
+		}
+		if pb, _ := bstack[0].(*precBuf); pb != nil {
+			buf = append(buf, pb.buf...)
+		}
+	}
+	buf = append(buf, ')')
+
+	return buf
+}
+
+// String representation of the script.
+func (s *Script) String() string {
+	return string(s.Append([]byte{}))
+}
+
+// Match returns true if the script returns true when evaluated against the
+// data argument.
+func (s *Script) Match(data any) bool {
+	stack := []any{}
+	if node, ok := data.(gen.Node); ok {
+		stack, _ = s.EvalWithRoot(stack, gen.Array{node}, data).([]any)
+	} else {
+		stack, _ = s.EvalWithRoot(stack, []any{data}, data).([]any)
+	}
+	return 0 < len(stack)
+}
+
+// Eval is primarily used by the Expr parser but is public for testing.
+func (s *Script) Eval(stack any, data any) any {
+	return s.EvalWithRoot(stack, data, nil)
+}
+
+// EvalWithRoot is primarily used by the Expr parser but is public for testing.
+func (s *Script) EvalWithRoot(stack any, data, root any) any {
+	// Checking the type each iteration adds 2.5% but allows code not to be
+	// duplicated and not to call a separate function. Using just one more
+	// function call for each iteration adds 6.5%.
+	var dlen int
+	switch td := data.(type) {
+	case []any:
+		dlen = len(td)
+	case gen.Array:
+		dlen = len(td)
+	case map[string]any:
+		dlen = len(td)
+		da := make([]any, 0, dlen)
+		for _, v := range td {
+			da = append(da, v)
+		}
+		data = da
+	case gen.Object:
+		dlen = len(td)
+		da := make(gen.Array, 0, dlen)
+		for _, v := range td {
+			da = append(da, v)
+		}
+		data = da
+	default:
+		rv := reflect.ValueOf(td)
+		if rv.Kind() != reflect.Slice && rv.Kind() != reflect.Array {
+			return stack
+		}
+		dlen = rv.Len()
+		da := make([]any, 0, dlen)
+		for i := 0; i < dlen; i++ {
+			da = append(da, rv.Index(i).Interface())
+		}
+		data = da
+	}
+	sstack := make([]any, len(s.template))
+	var v any
+	for vi := dlen - 1; 0 <= vi; vi-- {
+		switch td := data.(type) {
+		case []any:
+			v = td[vi]
+		case gen.Array:
+			v = td[vi]
+		}
+		// Eval script for each member of the list.
+		copy(sstack, s.template)
+		
