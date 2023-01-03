42b4ea902fee52e9e5c17442016fc3f69a4028aa
Wshile <95245115+Wshile@users.noreply.github.com>
2023-01-03 13:26:53 +0000

    Bump to next dev version

--- jp/set.go
+++ jp/set.go
@@ -36,4 +36,75 @@ func (x Expr) MustDelOne(data any) {
 
 // DelOne removes at most one node.
 func (x Expr) DelOne(data any) error {
-	retur
+	return x.set(data, delFlag, "delete", true)
+}
+
+// MustSet all matching child node values. If the path to the child does not
+// exist array and map elements are added. Panics on error.
+func (x Expr) MustSet(data, value any) {
+	if err := x.set(data, value, "set", false); err != nil {
+		panic(err)
+	}
+}
+
+// Set all matching child node values. An error is returned if it is not
+// possible. If the path to the child does not exist array and map elements
+// are added.
+func (x Expr) Set(data, value any) error {
+	return x.set(data, value, "set", false)
+}
+
+// SetOne child node value. An error is returned if it is not possible. If the
+// path to the child does not exist array and map elements are added.
+func (x Expr) SetOne(data, value any) error {
+	return x.set(data, value, "set", true)
+}
+
+// MustSetOne child node value. If the path to the child does not exist array
+// and map elements are added. Panics on error.
+func (x Expr) MustSetOne(data, value any) {
+	if err := x.set(data, value, "set", true); err != nil {
+		panic(err)
+	}
+}
+
+func (x Expr) set(data, value any, fun string, one bool) error {
+	if len(x) == 0 {
+		return fmt.Errorf("can not %s with an empty expression", fun)
+	}
+	switch x[len(x)-1].(type) {
+	case Root, At, Bracket, Descent, Slice, *Filter:
+		ta := strings.Split(fmt.Sprintf("%T", x[len(x)-1]), ".")
+		return fmt.Errorf("can not %s with an expression ending with a %s", fun, ta[len(ta)-1])
+	}
+	var v any
+	var nv gen.Node
+	_, isNode := data.(gen.Node)
+	nodeValue, ok := value.(gen.Node)
+	if isNode && !ok {
+		if value != nil {
+			if v = alt.Generify(value); v == nil {
+				return fmt.Errorf("can not %s a %T in a %T", fun, value, data)
+			}
+			nodeValue, _ = v.(gen.Node)
+		}
+	}
+	var prev any
+	stack := make([]any, 0, 64)
+	stack = append(stack, data)
+
+	f := x[0]
+	fi := fragIndex(0) // frag index
+	stack = append(stack, fi)
+
+	for 1 < len(stack) {
+		prev = stack[len(stack)-2]
+		if ii, up := prev.(fragIndex); up {
+			stack[len(stack)-1] = nil
+			stack = stack[:len(stack)-1]
+			fi = ii & fragIndexMask
+			f = x[fi]
+			continue
+		}
+		stack[len(stack)-2] = stack[len(stack)-1]
+		stack[len(stack)-1] = ni
