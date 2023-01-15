f7c6bb6ba9f45818fbf3fc8f25eddfd16f1da9f1
Wshile <95245115+Wshile@users.noreply.github.com>
2023-01-15 18:34:51 +0000

    Adding test file

--- oj/fint64.go
+++ oj/fint64.go
@@ -26,4 +26,72 @@ func appendInt64(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe boo
 	return buf, nil, aWrote
 }
 
-func appendInt64AsString(fi *finfo, buf []byte, rv reflect.Value, addr
+func appendInt64AsString(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	buf = append(buf, fi.jkey...)
+	buf = append(buf, '"')
+	buf = strconv.AppendInt(buf, *(*int64)(unsafe.Pointer(addr + fi.offset)), 10)
+	buf = append(buf, '"')
+
+	return buf, nil, aWrote
+}
+
+func appendInt64NotEmpty(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	v := *(*int64)(unsafe.Pointer(addr + fi.offset))
+	if v == 0 {
+		return buf, nil, aSkip
+	}
+	buf = append(buf, fi.jkey...)
+	buf = strconv.AppendInt(buf, v, 10)
+
+	return buf, nil, aWrote
+}
+
+func appendInt64NotEmptyAsString(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	v := *(*int64)(unsafe.Pointer(addr + fi.offset))
+	if v == 0 {
+		return buf, nil, aSkip
+	}
+	buf = append(buf, fi.jkey...)
+	buf = append(buf, '"')
+	buf = strconv.AppendInt(buf, v, 10)
+	buf = append(buf, '"')
+
+	return buf, nil, aWrote
+}
+
+func iappendInt64(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	buf = append(buf, fi.jkey...)
+	buf = strconv.AppendInt(buf, rv.FieldByIndex(fi.index).Interface().(int64), 10)
+
+	return buf, nil, aWrote
+}
+
+func iappendInt64AsString(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	buf = append(buf, fi.jkey...)
+	buf = append(buf, '"')
+	buf = strconv.AppendInt(buf, rv.FieldByIndex(fi.index).Interface().(int64), 10)
+	buf = append(buf, '"')
+
+	return buf, nil, aWrote
+}
+
+func iappendInt64NotEmpty(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	v := rv.FieldByIndex(fi.index).Interface().(int64)
+	if v == 0 {
+		return buf, nil, aSkip
+	}
+	buf = append(buf, fi.jkey...)
+	buf = strconv.AppendInt(buf, v, 10)
+
+	return buf, nil, aWrote
+}
+
+func iappendInt64NotEmptyAsString(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	v := rv.FieldByIndex(fi.index).Interface().(int64)
+	if v == 0 {
+		return buf, nil, aSkip
+	}
+	buf = append(buf, fi.jkey...)
+	buf = append(buf, '"')
+	buf = strconv.AppendInt(buf, v, 10)
+	buf = 
