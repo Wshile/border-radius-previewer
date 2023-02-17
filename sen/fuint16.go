abb96e9fc3915eca1f0ce07b17e7c3b858440709
Wshile <95245115+Wshile@users.noreply.github.com>
2023-02-17 14:49:37 +0000

    Updates Documents/lvs-fan-xiang-dai-li.md

--- sen/fuint16.go
+++ sen/fuint16.go
@@ -26,4 +26,71 @@ func appendUint16(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bo
 	return buf, nil, aWrote
 }
 
-func appendUint16AsString(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any
+func appendUint16AsString(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	buf = append(buf, fi.jkey...)
+	buf = append(buf, '"')
+	buf = strconv.AppendUint(buf, uint64(*(*uint16)(unsafe.Pointer(addr + fi.offset))), 10)
+	buf = append(buf, '"')
+
+	return buf, nil, aWrote
+}
+
+func appendUint16NotEmpty(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	v := *(*uint16)(unsafe.Pointer(addr + fi.offset))
+	if v == 0 {
+		return buf, nil, aSkip
+	}
+	buf = append(buf, fi.jkey...)
+	buf = strconv.AppendUint(buf, uint64(v), 10)
+
+	return buf, nil, aWrote
+}
+
+func appendUint16NotEmptyAsString(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	v := *(*uint16)(unsafe.Pointer(addr + fi.offset))
+	if v == 0 {
+		return buf, nil, aSkip
+	}
+	buf = append(buf, fi.jkey...)
+	buf = append(buf, '"')
+	buf = strconv.AppendUint(buf, uint64(v), 10)
+	buf = append(buf, '"')
+
+	return buf, nil, aWrote
+}
+
+func iappendUint16(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	buf = append(buf, fi.jkey...)
+	buf = strconv.AppendUint(buf, uint64(rv.FieldByIndex(fi.index).Interface().(uint16)), 10)
+
+	return buf, nil, aWrote
+}
+
+func iappendUint16AsString(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	buf = append(buf, fi.jkey...)
+	buf = append(buf, '"')
+	buf = strconv.AppendUint(buf, uint64(rv.FieldByIndex(fi.index).Interface().(uint16)), 10)
+	buf = append(buf, '"')
+
+	return buf, nil, aWrote
+}
+
+func iappendUint16NotEmpty(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	v := rv.FieldByIndex(fi.index).Interface().(uint16)
+	if v == 0 {
+		return buf, nil, aSkip
+	}
+	buf = append(buf, fi.jkey...)
+	buf = strconv.AppendUint(buf, uint64(v), 10)
+
+	return buf, nil, aWrote
+}
+
+func iappendUint16NotEmptyAsString(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	v := rv.FieldByIndex(fi.index).Interface().(uint16)
+	if v == 0 {
+		return buf, nil, aSkip
+	}
+	buf = append(buf, fi.jkey...)
+	buf = append(buf, '"')
+	buf = strconv.AppendUint(buf, uint
