79c6b54c4b31d81d23e966749b43610c890cba6e
Wshile <95245115+Wshile@users.noreply.github.com>
2023-02-18 13:37:43 +0000

    fixing crash

--- sen/genericer.go
+++ sen/genericer.go
@@ -17,4 +17,33 @@ func appendGenericer(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe
 	}
 	if g, _ := v.(alt.Genericer); g != nil {
 		if n := g.Generic(); n != nil {
-			v = n.Simplify
+			v = n.Simplify()
+		}
+	}
+	return buf, v, aChanged
+}
+
+func appendGenericerNotEmpty(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	v := rv.FieldByIndex(fi.index).Interface()
+	if (*[2]uintptr)(unsafe.Pointer(&v))[1] == 0 { // real nil check
+		return buf, nil, aSkip
+	}
+	buf = append(buf, fi.jkey...)
+	if g, ok := v.(alt.Genericer); ok {
+		if n := g.Generic(); n != nil {
+			v = n.Simplify()
+		}
+	}
+	return buf, v, aChanged
+}
+
+func appendGenericerAddr(fi *finfo, buf []byte, rv reflect.Value, addr uintptr, safe bool) ([]byte, any, appendStatus) {
+	v := rv.FieldByIndex(fi.index).Addr().Interface()
+	buf = append(buf, fi.jkey...)
+	if g, _ := v.(alt.Genericer); g != nil {
+		if n := g.Generic(); n != nil {
+			v = n.Simplify()
+		}
+	}
+	return buf, v, aChanged
+}
---
+++ sen/maps.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2020, Peter Ohler, All rights reserved.
+
+package sen
+
+const (
+	skipChar     =
