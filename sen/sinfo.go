50f562d7828bbc075cc3288ca9bf480a67329182
Wshile <95245115+Wshile@users.noreply.github.com>
2023-02-24 07:20:14 +0000

    api

--- sen/sinfo.go
+++ sen/sinfo.go
@@ -99,4 +99,81 @@ func buildTagFields(rt reflect.Type, out, pretty, embedded, omitEmpty bool) (fa 
 					fa = append(fa, fi)
 				}
 			} else {
-				f
+				for _, fi := range buildTagFields(f.Type, out, pretty, embedded, omitEmpty) {
+					fi.index = append([]int{i}, fi.index...)
+					fi.offset += f.Offset
+					fa = append(fa, fi)
+				}
+			}
+		} else {
+			asString := false
+			key := f.Name
+			if tag, ok := f.Tag.Lookup("json"); ok && 0 < len(tag) {
+				parts := strings.Split(tag, ",")
+				switch parts[0] {
+				case "":
+					key = f.Name
+				case "-":
+					if 1 < len(parts) {
+						key = "-"
+					} else {
+						continue
+					}
+				default:
+					key = parts[0]
+				}
+				for _, p := range parts[1:] {
+					switch p {
+					case "omitempty":
+						omitEmpty = true
+					case "string":
+						asString = true
+					}
+				}
+			}
+			fa = append(fa, newFinfo(&f, key, omitEmpty, asString, pretty, embedded))
+		}
+	}
+	return
+}
+
+func buildExactFields(rt reflect.Type, out, pretty, embedded, omitEmpty bool) (fa []*finfo) {
+	for i := rt.NumField() - 1; 0 <= i; i-- {
+		f := rt.Field(i)
+		name := []byte(f.Name)
+		if len(name) == 0 || 'a' <= name[0] {
+			continue
+		}
+		if f.Anonymous && !out {
+			if f.Type.Kind() == reflect.Ptr {
+				for _, fi := range buildExactFields(f.Type.Elem(), out, pretty, embedded, omitEmpty) {
+					fi.index = append([]int{i}, fi.index...)
+					fi.Append = fi.iAppend
+					fa = append(fa, fi)
+				}
+			} else {
+				for _, fi := range buildExactFields(f.Type, out, pretty, embedded, omitEmpty) {
+					fi.index = append([]int{i}, fi.index...)
+					fi.offset += f.Offset
+					fa = append(fa, fi)
+				}
+			}
+		} else {
+			fa = append(fa, newFinfo(&f, f.Name, omitEmpty, false, pretty, embedded))
+		}
+	}
+	return
+}
+
+func buildLowFields(rt reflect.Type, out, pretty, embedded, omitEmpty bool) (fa []*finfo) {
+	for i := rt.NumField() - 1; 0 <= i; i-- {
+		f := rt.Field(i)
+		name := []byte(f.Name)
+		if len(name) == 0 || 'a' <= name[0] {
+			continue
+		}
+		if f.Anonymous && !out {
+			if f.Type.Kind() == reflect.Ptr {
+				for _, fi := range buildLowFields(f.Type.Elem(), out, pretty, embedded, omitEmpty) {
+					fi.index = append([]int{i}, fi.index...)
+					fi.App
