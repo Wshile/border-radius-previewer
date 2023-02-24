9df9d2aea6ba6a8191dc8895f2cdd0ee315c33e4
Wshile <95245115+Wshile@users.noreply.github.com>
2023-02-24 06:14:04 +0000

    git push

--- sen/sinfo.go
+++ sen/sinfo.go
@@ -24,4 +24,79 @@ type sinfo struct {
 	fields [16][]*finfo
 }
 
-v
+var (
+	structMut sync.Mutex
+	// Keyed by the pointer to the type.
+	structMap      = map[uintptr]*sinfo{}
+	structEmptyMap = map[uintptr]*sinfo{}
+)
+
+// Non-locking version used in field creation.
+func getTypeStruct(rt reflect.Type, embedded, omitEmpty bool) (st *sinfo) {
+	x := (*[2]uintptr)(unsafe.Pointer(&rt))[1]
+	if st = structMap[x]; st != nil {
+		return
+	}
+	return buildStruct(rt, x, embedded, omitEmpty)
+}
+
+func getSinfo(v any, omitEmpty bool) (st *sinfo) {
+	x := (*[2]uintptr)(unsafe.Pointer(&v))[0]
+	sm := structMap
+	if omitEmpty {
+		sm = structEmptyMap
+	}
+	structMut.Lock()
+	defer structMut.Unlock()
+	if st = sm[x]; st != nil {
+		return
+	}
+	return buildStruct(reflect.TypeOf(v), x, false, omitEmpty)
+}
+
+func buildStruct(rt reflect.Type, x uintptr, embedded, omitEmpty bool) (st *sinfo) {
+	st = &sinfo{rt: rt}
+	if omitEmpty {
+		structEmptyMap[x] = st
+	} else {
+		structMap[x] = st
+	}
+	for u := byte(0); u < maskMax; u++ {
+		if (maskByTag&u) != 0 && (maskExact&u) != 0 { // reuse previously built
+			st.fields[u] = st.fields[u & ^maskExact]
+			continue
+		}
+		st.fields[u] = buildFields(st.rt, u, embedded, omitEmpty)
+	}
+	return
+}
+
+func buildFields(rt reflect.Type, u byte, embedded, omitEmpty bool) (fa []*finfo) {
+	switch {
+	case (maskByTag & u) != 0:
+		fa = buildTagFields(rt, (maskNested&u) != 0, (maskPretty&u) != 0, embedded, omitEmpty)
+	case (maskExact & u) != 0:
+		fa = buildExactFields(rt, (maskNested&u) != 0, (maskPretty&u) != 0, embedded, omitEmpty)
+	default:
+		fa = buildLowFields(rt, (maskNested&u) != 0, (maskPretty&u) != 0, embedded, omitEmpty)
+	}
+	sort.Slice(fa, func(i, j int) bool { return 0 > strings.Compare(fa[i].key, fa[j].key) })
+	return
+}
+
+func buildTagFields(rt reflect.Type, out, pretty, embedded, omitEmpty bool) (fa []*finfo) {
+	for i := rt.NumField() - 1; 0 <= i; i-- {
+		f := rt.Field(i)
+		name := []byte(f.Name)
+		if len(name) == 0 || 'a' <= name[0] {
+			continue
+		}
+		if f.Anonymous && !out {
+			if f.Type.Kind() == reflect.Ptr {
+				for _, fi := range buildTagFields(f.Type.Elem(), out, pretty, embedded, omitEmpty) {
+					fi.index = append([]int{i}, fi.index...)
+					fi.Append = fi.iAppend
+					fa = append(fa, fi)
+				}
+			} else {
+				f
