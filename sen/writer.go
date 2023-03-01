30f5e93c1fee33e8edbc31e0781875dec240b4b4
Wshile <95245115+Wshile@users.noreply.github.com>
2023-03-01 11:02:09 +0000

    more readme updates

--- sen/writer.go
+++ sen/writer.go
@@ -92,4 +92,104 @@ func (wr *Writer) MustSEN(data any) []byte {
 }
 
 // Write a SEN string for the data provided.
-func (wr *Writer) Write(w io.Writer, data any) (err e
+func (wr *Writer) Write(w io.Writer, data any) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			wr.buf = wr.buf[:0]
+			err = ojg.NewError(r)
+		}
+	}()
+	wr.MustWrite(w, data)
+	return
+}
+
+// MustWrite a SEN string for the data provided. If an error occurs panic is
+// called with the error.
+func (wr *Writer) MustWrite(w io.Writer, data any) {
+	wr.w = w
+	if wr.InitSize <= 0 {
+		wr.InitSize = 256
+	}
+	if wr.WriteLimit <= 0 {
+		wr.WriteLimit = 1024
+	}
+	if cap(wr.buf) < wr.InitSize {
+		wr.buf = make([]byte, 0, wr.InitSize)
+	} else {
+		wr.buf = wr.buf[:0]
+	}
+	wr.calcFieldsIndex()
+	if wr.Color {
+		wr.colorSEN(data, 0)
+	} else {
+		wr.appendString = ojg.AppendSENString
+		if wr.Tab || 0 < wr.Indent {
+			wr.appendArray = appendArray
+			if wr.Sort {
+				wr.appendObject = appendSortObject
+			} else {
+				wr.appendObject = appendObject
+			}
+			wr.appendDefault = appendDefault
+		} else {
+			wr.appendArray = tightArray
+			if wr.Sort {
+				wr.appendObject = tightSortObject
+			} else {
+				wr.appendObject = tightObject
+			}
+			wr.appendDefault = tightDefault
+		}
+		wr.appendSEN(data, 0)
+	}
+	if 0 < len(wr.buf) {
+		if _, err := wr.w.Write(wr.buf); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func (wr *Writer) calcFieldsIndex() {
+	wr.findex = 0
+	if wr.NestEmbed {
+		wr.findex |= maskNested
+	}
+	if 0 < wr.Indent {
+		wr.findex |= maskPretty
+	}
+	if wr.UseTags {
+		wr.findex |= maskByTag
+	} else if wr.KeyExact {
+		wr.findex |= maskExact
+	}
+}
+
+func (wr *Writer) appendSEN(data any, depth int) {
+	wr.needSep = true
+	switch td := data.(type) {
+	case nil:
+		wr.buf = append(wr.buf, "null"...)
+
+	case bool:
+		if td {
+			wr.buf = append(wr.buf, "true"...)
+		} else {
+			wr.buf = append(wr.buf, "false"...)
+		}
+
+	case int:
+		wr.buf = strconv.AppendInt(wr.buf, int64(td), 10)
+	case int8:
+		wr.buf = strconv.AppendInt(wr.buf, int64(td), 10)
+	case int16:
+		wr.buf = strconv.AppendInt(wr.buf, int64(td), 10)
+	case int32:
+		wr.buf = strconv.AppendInt(wr.buf, int64(td), 10)
+	case int64:
+		wr.buf = strconv.AppendInt(wr.buf, td, 10)
+	case uint:
+		wr.buf = strconv.AppendUint(wr.buf, uint64(td), 10)
+	case uint8:
+		wr.buf = strconv.AppendUint(wr.buf, uint64(td), 10)
+	case uint16:
+		wr.buf = strconv.AppendUi
